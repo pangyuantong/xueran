bff0b5316a433b3952e23f2b182c422729ee5875
WallacePangYT <136036130+WallacePangYT@users.noreply.github.com>
2024-03-13 15:00:10 +0800

    Init

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        // path: "/:id",
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        action: dashboardAction,
+        errorElement: <Error />,
+      },
+      {
+        path: "*",
+        element: <Error />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
