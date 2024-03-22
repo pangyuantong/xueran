1cad95fe1d8f16d2033ef87800c461d2f953aa4a
pangyuantong <pangyuantong1026@gmail.com>
2024-03-23 04:50:40 +0800

    Full Flow Mock

--- src/components/PowerCard.jsx
+++ src/components/PowerCard.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { fetchData } from "../helpers";
 
-const PowerCard = ({seatNum}) => {
+const PowerCard = ({ seatNum }) => {
   const [isFlipped, setIsFlipped] = useState(false);
+  const roleInfo = fetchData("roleInfo");
   const handleClick = () => {
     setIsFlipped(!isFlipped); // Toggle the isFlipped state
   };
 
   useEffect(() => {
     // If either `loggedUser` or `_token` exists, set loading to false
-    
   }, []);
 
   return (
@@ -21,7 +22,26 @@ const PowerCard = ({seatNum}) => {
           <div className="front">
             <h1>{seatNum}</h1>
           </div>
-          <div className="back">{/* Back side content */}</div>
+          <div className="back">
+            {roleInfo && (
+              <>
+                <div className="role-name">{roleInfo.roleName}</div>
+                <img
+                  src={roleInfo.roleImg}
+                  alt="Character"
+                  className="character-image"
+                />
+                <p className="description">{roleInfo.roleDesc}</p>
+                <div className="category">
+                  {roleInfo.roleFaction === 0 && '镇民'}
+                  {roleInfo.roleFaction === 1 && '外来者'}
+                  {roleInfo.roleFaction === 2 && '爪牙'}
+                  {roleInfo.roleFaction === 3 && '恶魔'}
+                  {roleInfo.roleFaction === 4 && '旅行者'}
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
