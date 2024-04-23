b20078483c711a9083aa8176de2bac28a261233f
pangyuantong <pangyuantong1026@gmail.com>
2024-04-23 17:14:25 +0800

    Checkpoint 23 APR

--- src/components/PlayerSeats.jsx
+++ src/components/PlayerSeats.jsx
@@ -8,6 +8,7 @@ import {
   ListGroup,
   Tab,
   Tabs,
+  Col,
 } from "react-bootstrap";
 import { fetchData, viewPlayers } from "../helpers";
 
@@ -19,23 +20,30 @@ const PlayerSeats = ({ capacity, user }) => {
 
   // State to manage badge visibility
   const [demons, setDemons] = useState([]);
+  const [pickMode, setPickMode] = useState(false);
 
   // Function to toggle the visibility of the badge
   const toggleSeat = (id) => {
-    if (demons.includes(id)) {
-      // If the id is already in the demons array, remove it
-      setDemons(demons.filter((demon) => demon !== id));
-    } else {
-      // If the id is not in the demons array, add it
-      let updatedDemons = [...demons, id];
+    if (pickMode === true) {
+      if (demons.includes(id)) {
+        // If the id is already in the demons array, remove it
+        setDemons(demons.filter((demon) => demon !== id));
+      } else {
+        // If the id is not in the demons array, add it
+        let updatedDemons = [...demons, id];
 
-      // Check if the updated array length exceeds 3
-      if (updatedDemons.length > 3) {
-        // Remove the first element
-        updatedDemons.shift();
+        // Check if the updated array length exceeds 3
+        if (updatedDemons.length > 3) {
+          // Remove the first element
+          updatedDemons.shift();
+        }
+        setDemons(updatedDemons);
       }
-      setDemons(updatedDemons);
     }
+  };
+
+  const togglePickMode = () => {
+    setPickMode(!pickMode);
   };
 
   const LeftSeat = ({ seatNumber, seatInfo }) => (
@@ -96,10 +104,11 @@ const PlayerSeats = ({ capacity, user }) => {
           setSeatData(res.data);
           console.log(seatData);
           console.log("test");
+          setLoading(false);
         } else {
           toast.error("Oops! " + res.message);
+          setLoading(false);
         }
-        setLoading(false);
       } catch (e) {
         setLoading(false);
         console.error("Error retrieving data:", e);
@@ -111,11 +120,11 @@ const PlayerSeats = ({ capacity, user }) => {
 
   return (
     <div className="my-3">
-      {leftCount < 1 ? (
+      {leftCount < 1 || loading === true ? (
         <Container
           fluid
           style={{
-            height: "100vh",
+            height: "50vh",
             maxWidth: "100vw",
             display: "flex",
             alignItems: "center",
@@ -126,6 +135,33 @@ const PlayerSeats = ({ capacity, user }) => {
         </Container>
       ) : (
         <div>
+          <Row className="mx-0 mb-3 p-0" style={{ maxWidth: "100vw" }}>
+            <Col className="d-flex justify-content-center">
+              <Button
+                className="btn-danger"
+                style={{
+                  width: "45px",
+                  height: "45px",
+                  borderRadius: "50%",
+                  padding: "0",
+                }}
+                onClick={togglePickMode}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="currentColor"
+                  class="bi bi-crosshair"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8.5.5a.5.5 0 0 0-1 0v.518A7 7 0 0 0 1.018 7.5H.5a.5.5 0 0 0 0 1h.518A7 7 0 0 0 7.5 14.982v.518a.5.5 0 0 0 1 0v-.518A7 7 0 0 0 14.982 8.5h.518a.5.5 0 0 0 0-1h-.518A7 7 0 0 0 8.5 1.018zm-6.48 7A6 6 0 0 1 7.5 2.02v.48a.5.5 0 0 0 1 0v-.48a6 6 0 0 1 5.48 5.48h-.48a.5.5 0 0 0 0 1h.48a6 6 0 0 1-5.48 5.48v-.48a.5.5 0 0 0-1 0v.48A6 6 0 0 1 2.02 8.5h.48a.5.5 0 0 0 0-1zM8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                </svg>
+              </Button>
+            </Col>
+            <Col></Col>
+            <Col></Col>
+          </Row>
           <Row style={{ paddingInline: "3%" }}>
             <div className="col-6 cust-seat">
               <ListGroup style={{ paddingLeft: "10px" }}>
@@ -160,22 +196,6 @@ const PlayerSeats = ({ capacity, user }) => {
                     })}
               </ListGroup>
             </div>
-          </Row>
-
-          <Row className="d-flex justify-content-center">
-            <Button className="btn-danger" style={{ width: "auto", height:"50px", borderRadius:"30%" }}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-crosshair"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8.5.5a.5.5 0 0 0-1 0v.518A7 7 0 0 0 1.018 7.5H.5a.5.5 0 0 0 0 1h.518A7 7 0 0 0 7.5 14.982v.518a.5.5 0 0 0 1 0v-.518A7 7 0 0 0 14.982 8.5h.518a.5.5 0 0 0 0-1h-.518A7 7 0 0 0 8.5 1.018zm-6.48 7A6 6 0 0 1 7.5 2.02v.48a.5.5 0 0 0 1 0v-.48a6 6 0 0 1 5.48 5.48h-.48a.5.5 0 0 0 0 1h.48a6 6 0 0 1-5.48 5.48v-.48a.5.5 0 0 0-1 0v.48A6 6 0 0 1 2.02 8.5h.48a.5.5 0 0 0 0-1zM8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
-              </svg>
-              狙击
-            </Button>
           </Row>
         </div>
       )}
